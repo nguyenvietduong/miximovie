d3f4fb4f2ea4e60b40622f27355ec5841d4b55bf
nguyenvietduong <duonnv10504@gmail.com>
2025-09-26 01:13:56 +0700

    Duong Update

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,154 @@
+<?php
+
+// Chi tiết: https://rophimapi.net/v1/movie/detail/Uy9duYTj
+// Link Phim: https://goatembed.com/w2g/KLZQJN2l?start_time=1758822764000000&amp;token=U2FsdGVkX1%2Bhb6RIc%2BAjXjQn9fw9k7A3ShaUs7s3HdEy%2FLOJ6236HooZ0JWTan8i
+
+// {
+//   "status": true,
+//   "msg": "Thành công.",
+//   "result": {
+//     "_id": "Uy9duYTj",
+//     "original_title": "ONE PIECE “3D2Y” エースの死を越えて! ルフィ仲間との誓い",
+//     "title": "Đảo Hải Tặc: 3D2Y: Vượt Qua Cái Chết Của Ace! Lời Hứa Của Luffy Với Đồng Đội",
+//     "slug": "dao-hai-tac-3d2y-vuot-qua-cai-chet-cua-ace-loi-hua-cua-luffy-voi-dong-doi",
+//     "overview": "Trong trận Marineford (Tổng Bộ Hải Quân), Luffy đau khổ khi mất đi người anh trai của mình – Ace và cậu nhận ra rằng mình vẫn còn những người đồng đội (nakama). Để vượt qua nỗi buồn và quyết tâm mạnh hơn nữa để bảo vệ đồng đội mình, cậu đã nhờ Rayleigh chỉ bảo và dạy cậu cách để mạnh hơn. Quá trình tập luyện của Luffy kéo dài 2 năm tại đảo Rusukaina.\r\nLuffy sẽ được luyện tập cùng Rayleigh cũng như cách sử dụng Haki trong tập đặc biệt này.",
+//     "release_date": "2014-12-31",
+//     "rating": "K",
+//     "runtime": 105,
+//     "type": 1,
+//     "origin_country": [
+//       "JP"
+//     ],
+//     "attribute_ids": [
+//       "cCpBXO",
+//       "BYKghI",
+//       "qdIgum"
+//     ],
+//     "production_company_ids": [
+//       "7FQkZN",
+//       "fFdsPW"
+//     ],
+//     "network_ids": [],
+//     "director_ids": [
+//       "ChCcCW",
+//       "ITE44g",
+//       "vsAqXH"
+//     ],
+//     "status": "Released",
+//     "publish": true,
+//     "is_private": false,
+//     "english_title": "One Piece: 3D2Y - Overcome Ace's Death! Luffy's Vow to His Friends",
+//     "imdb_rating": "7.5",
+//     "quality": "fhd",
+//     "latest_episode": {
+//       "1": 1
+//     },
+//     "public_id": "Uy9duYTj",
+//     "year": "2014",
+//     "countries": [
+//       {
+//         "_id": "vPmCDj",
+//         "name": "Nhật Bản",
+//         "slug": "nhat-ban"
+//       }
+//     ],
+//     "production_companies": [
+//       {
+//         "_id": "7FQkZN",
+//         "name": "Toei Company",
+//         "slug": "toei-company"
+//       },
+//       {
+//         "_id": "fFdsPW",
+//         "name": "Toei Animation",
+//         "slug": "toei-animation"
+//       }
+//     ],
+//     "networks": [],
+//     "directors": [
+//       {
+//         "_id": "ChCcCW",
+//         "name": "Yukihiko Nakao",
+//         "slug": "yukihiko-nakao"
+//       },
+//       {
+//         "_id": "ITE44g",
+//         "name": "Aya Komaki",
+//         "slug": "aya-komaki"
+//       },
+//       {
+//         "_id": "vsAqXH",
+//         "name": "Naoyuki Ito",
+//         "slug": "naoyuki-ito"
+//       }
+//     ],
+//     "attributes": [
+//       {
+//         "_id": "BYKghI",
+//         "name": "Khám Phá",
+//         "slug": "kham-pha"
+//       },
+//       {
+//         "_id": "cCpBXO",
+//         "name": "Bạo Lực",
+//         "slug": "bao-luc"
+//       },
+//       {
+//         "_id": "qdIgum",
+//         "name": "Hài Hước",
+//         "slug": "hai-huoc"
+//       }
+//     ],
+//     "genres": [
+//       {
+//         "_id": "2xCjTG",
+//         "name": "Hành Động",
+//         "slug": "hanh-dong"
+//       },
+//       {
+//         "_id": "W8Dn2a",
+//         "name": "Hài",
+//         "slug": "hai"
+//       },
+//       {
+//         "_id": "ZERgfX",
+//         "name": "Hoạt Hình",
+//         "slug": "hoat-hinh"
+//       },
+//       {
+//         "_id": "wN4Ysm",
+//         "name": "Viễn Tưởng",
+//         "slug": "vien-tuong"
+//       },
+//       {
+//         "_id": "wca3Bp",
+//         "name": "Phiêu Lưu",
+//         "slug": "phieu-luu"
+//       }
+//     ],
+//     "images": {
+//       "posters": [
+//         {
+//           "_id": "683bd3f63189360ab3628c4d",
+//           "path": "6c/52/6c52baba5dbcf88c71a317dd2753c249/6c52baba5dbcf88c71a317dd2753c249.webp",
+//           "type": 1
+//         }
+//       ],
+//       "horizontal_posters": [
+//         {
+//           "_id": "683bd3ff68105005bb7186d9",
+//           "path": "99/a5/99a53b1fbb4b8649b782666bae7e69a5/99a53b1fbb4b8649b782666bae7e69a5.jpg",
+//           "type": 2
+//         }
+//       ],
+//       "backdrops": [
+//         {
+//           "_id": "683bd40b3189360ab3628d12",
+//           "path": "c8/89/c889a4b47104db9269a33e926aa8ca4f/c889a4b47104db9269a33e926aa8ca4f.webp",
+//           "type": 3
+//         }
+//       ],
+//       "titles": []
+//     }
+//   }
+// }
